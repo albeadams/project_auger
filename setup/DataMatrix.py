3e1b28373d01c3efb9f8b9e8a86cd02997b395e7
Adams <albeadam@helenaagri.com>
2019-10-04 15:06:19 -0500

    removed getters and setters for pythonic

--- setup/DataMatrix.py
+++ setup/DataMatrix.py
@@ -8,38 +8,12 @@ class DataMatrix(object):
     self.datafile = datafile
     self.df = pd.read_csv(directory+datafile, sep=',', na_values=['?'])
     self.header = None
-
-  def store_train_test_data(self, train_set=None, test_set=None):
-    self.train_set = train_set
-    self.test_set = test_set
-    self.train_set_copy = self.train_set.copy()
+    self.train_set = None
+    self.test_set = None
+    self.train_copy = None
 
   def store_missing_values(self, df=None, choice=None):
     pass
 
-  # getters and setters
-  def get_dataframe(self):
-    return self.df
-
   def get_numpy_array(self):
     return self.df.to_numpy()
-
-  def get_header(self):
-    return self.header
-
-  def set_dataframe(self, df):
-    self.df = df
-
-  def set_header(self, hasheader=True):
-    if hasheader:
-      self.header = self.df.columns.values
-    else:
-      self.header = None
-
-  # important to note - this returns copy of training set
-  def get_train_set(self):
-    return self.train_set_copy
-
-  # this updates the copy of the training set
-  def set_train_set(self, df=None):
-    self.train_set_copy = df
--- setup/RunAuger.py
+++ setup/RunAuger.py
@@ -20,20 +20,19 @@ class RunAuger(object):
     while True:
       hh = self.inp.get_input("Does your data have column headers? (y or n): ")
       if hh == 'y':
-        self.dm.set_header(True)
+        self.dm.header = self.dm.df.columns.values
         break
       elif hh == 'n':
-        self.dm.set_header(False)
         break
       else:
         self.inp.print_out("Try again. Enter y for yes, n for no.")
 
     # split the dataframe in process, store it in datamatrxix
-    train_set, test_set = self.process.easy_split(self.dm.get_dataframe())
-    self.dm.store_train_test_data(train_set, test_set)
+    self.dm.train_set, self.dm.test_set = self.process.easy_split(self.dm.df)
+    self.dm.train_copy = self.dm.train_set.copy()
 
     # choose how to replace nan values
-    if self.process.check_for_nulls(self.dm.get_train_set()):
+    if self.process.check_for_nulls(self.dm.train_copy):
       print(info.missingvalues)
       while True:
         try:
@@ -51,9 +50,9 @@ class RunAuger(object):
       # get the train set, pass it and choice to fix_nulls, which returns the fixed df
       # and the result of the computation, i.e. if max was the choice, the result would return
       # this numeric value
-      #self.dm.store_missing_value(self.process.fix_nulls(self.dm.get_train_set(), choice), result)
+      self.dm.store_missing_value(self.process.fix_nulls(self.dm.train_copy, choice), result)
     else:
       print(info.nomissingfound)
-      #self.dm.store_missing_value(self.dm.get_train_set(), 0) # correct format for result?
+      self.dm.store_missing_value(self.dm.train_copy, 0) # correct format for result?
 
 
